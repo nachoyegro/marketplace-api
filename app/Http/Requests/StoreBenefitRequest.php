3949678d1f1a58caf7cc334b1398b775953f11f3
Juan Ignacio Yegro <jyegro@Juans-MacBook-Pro.local>
2025-06-16 20:29:23 -0300

    Fix StoreBenefitRequest error

--- app/Http/Requests/StoreBenefitRequest.php
+++ app/Http/Requests/StoreBenefitRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Benefit;
 
 class StoreBenefitRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreBenefitRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return auth()->user()->can('create', Order::class);
+        return auth()->user()->can('create', Benefit::class);
     }
 
     /**
